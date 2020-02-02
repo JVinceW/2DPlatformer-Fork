e8b67451563fb25b0c7e7c43c900c730550091d8
Vince Wang <vincewang10a7@gmail.com>
2020-02-02 17:03:02 +0900

    Scene shortcut generation script.

--- Assets/AppDevelopment/Editor/Generator/SceneMenuGenerator.cs
+++ Assets/AppDevelopment/Editor/Generator/SceneMenuGenerator.cs
@@ -2,9 +2,16 @@ using UnityEditor;
 
 namespace AppDevelopment.Editor.Generator {
     public static class SceneMenuGenerator {
+        public static readonly string K_CLASS_TEMPLATE = @"using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace AppDevelopment.Editor {
+    public static class SceneShortcutMenu {
+        $METHOD_ARRAY
+    }
+}";
+
         [MenuItem("Develop/Generator/Update Scene Menu")]
-        public static void GenerateSceneMenu() {
-            
-        }
+        public static void GenerateSceneMenu() { }
     }
 }
